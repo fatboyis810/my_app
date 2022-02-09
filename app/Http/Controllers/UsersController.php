064a778663792fd9908b000a184da69b3096a9ee
fatboy_pc <fatboy>
2022-02-09 19:24:19 +0900

    バリデート処理の変更並びにconfigファイルを作成、特定の処理を追加

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,213 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use App\common\CommonFunc;
-use Exception;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
-    private $commonFunc;
-    private $response;
-    public $allUserData;
-
-    public function __construct()
-    {
-        $this->commonFunc = new CommonFunc();
-        $this->response = [
-            'status' => 0,
-            'message' => '',
-            'result' => [],
-        ];
-    }
-
-    public function makeUser(Request $request): JsonResponse
-    {
-        try {
-            $validateRes = $this->commonFunc->apiNullValidate($request->all());
-            $validateAgeRes = $this->commonFunc->apiIntegerChecker($request->age);
-
-            if ($validateRes == false) {
-                $this->response['status'] = -1;
-                $this->response['message'] = '全項目を入力してください。';
-
-                return response()->json($this->response);
-            }
-
-            if ($validateAgeRes == false) {
-                $this->response['status'] = -1;
-                $this->response['message'] = '年齢を数字で入力してください';
-
-                return response()->json($this->response);
-            }
-
-            $user = new User();
-            $user->fill($request->all());
-            $user->password = bcrypt($request->password);
-
-            if ($user->save() == false) {
-                $this->response['status'] = -1;
-                $this->response['message'] = '予期せぬエラーが発生しました。';
-
-                return response()->json($this->response);
-            }
-
-            return response()->json($this->response);
-        } catch (Exception $exception) {
-
-            Log::error($exception);
-            $this->response['status'] = -1;
-            $this->response['message'] = '予期せぬエラーが発生しました。';
-
-            return response()->json($this->response);
-        }
-    }
-
-    public function userList(): JsonResponse
+    //表示用
+    public function index()
     {
         $users = User::all();
-        $result = [];
 
-        foreach ($users as $user) {
-            $result [] = [
-                'id' => $user->id,
-                'user_name' => $user->user_name,
-                'first_name' => $user->first_name,
-                'last_name' => $user->last_name,
-                'email' => $user->email,
-                'gender' => $user->gender,
-                'age' => $user->age,
-                'user_type' => $user->user_type,
-            ];
-        }
-
-        $this->response['result'] = $result;
-
-        return response()->json($this->response);
+        return view('user.index', compact('users'));
     }
 
-    public function userEdit($id): JsonResponse
+    //作成ページ表示用
+    public function create()
     {
-        $user = User::find($id);
-
-        if ($user == false) {
-            $this->response['status'] = -1;
-            $this->response['message'] = 'ユーザーが見つかりません';
-
-            return response()->json($this->response);
-        }
-
-        $result = [
-            'id' => $user->id,
-            'user_name' => $user->user_name,
-            'first_name' => $user->first_name,
-            'last_name' => $user->last_name,
-            'email' => $user->email,
-            'gender' => $user->gender,
-            'age' => $user->age,
-            'user_type' => $user->user_type,
-        ];
-
-        $this->response['result'] = $result;
-
-        return response()->json($this->response);
+        return view('user.create');
     }
 
-    public function updateUser(Request $request, $id): JsonResponse
+    //編集ページ表示用
+    public function edit(User $user)
     {
-        try {
-            $validateRes = $this->commonFunc->apiNullValidate($request->all());
-            $validateAgeRes = $this->commonFunc->apiIntegerChecker($request->age);
+        $genders = Config::get('my_app.genders');
 
-            if ($validateRes == false) {
-                $this->response['status'] = -1;
-                $this->response['message'] = '全項目を入力してください。';
-
-                return response()->json($this->response);
-            }
-
-            if ($validateAgeRes == false) {
-                $this->response['status'] = -1;
-                $this->response['message'] = '年齢を数字で入力してください';
-
-                return response()->json($this->response);
-            }
-
-            $user = User::find($id);
-            $user->fill($request->all());
-            $user->password = bcrypt($request->password);
-
-            if ($user->save() == false) {
-                $this->response['status'] = -1;
-                $this->response['message'] = '予期せぬエラーが発生しました。';
-
-                return response()->json($this->response);
-            }
-
-            return response()->json($this->response);
-        } catch (Exception $exception) {
-
-            Log::error($exception);
-            $this->response['status'] = -1;
-            $this->response['message'] = '予期せぬエラーが発生しました。';
-
-            return response()->json($this->response);
-        }
+        return view('user.edit', compact('user','genders'));
     }
 
-    public function deleteUser($id): JsonResponse
+    //新規作成
+    public function store(UserRequest $request)
     {
-        $user = User::find($id);
+        $user = (new User())->fill($request->except('password'));
+        $user->password = Hash::make($request->get('password'));
 
-        if ($user == false) {
-            $this->response['status'] = -1;
-            $this->response['message'] = 'ユーザーが見つかりません。';
+        if (!$user->save()){
 
-            return response()->json($this->response);
+            return back()
+                ->withErrors(['新規作成に失敗しました。'])
+                ->withInput();
         }
 
-        if ($user->delete() == false) {
-            $this->response['status'] = -1;
-            $this->response['message'] = '予期せぬエラーが発生しました。';
-
-            return response()->json($this->response);
-        }
-
-        return response()->json($this->response);
+        return redirect('/user');
     }
 
-    public function search(Request $request): JsonResponse
+    //更新用
+    public function update()
     {
-        $search = $request->input('search');
-        $result = [];
 
-        if ($search == null) {
-            $users = DB::table('users')->select('*')->get();
-        } else {
-            $users = DB::table('users')->select('*')
-                ->where('email', 'like', '%' . $search . '%')
-                ->orWhere('user_name', 'like', '%' . $search . '%')
-                ->get();
-        }
-
-        foreach ($users as $user) {
-            $result [] = [
-                'id' => $user->id,
-                'user_name' => $user->user_name,
-                'first_name' => $user->first_name,
-                'last_name' => $user->last_name,
-                'email' => $user->email,
-                'gender' => $user->gender,
-                'age' => $user->age,
-                'user_type' => $user->user_type,
-            ];
-        }
-
-        $this->response['result'] = $result;
-        return response()->json($this->response);
     }
 }
